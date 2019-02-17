d5fca925a68f1d41f57f4e96616903d3766791b9
Yacine Benouniche <benounicheyacine@gmail.com>
2019-02-16 23:48:23 -0500

    tests

--- TP1/test_puissance.py
+++ TP1/test_puissance.py
@@ -4,11 +4,25 @@ import seaborn as sns
 import sys
 import numpy
 
-df = pd.read_csv('resultats.csv').groupby(['algo','serie','taille']).mean().reset_index()
+df = pd.read_csv('resultats.csv').groupby(['algo', 'serie', 'taille']).mean().reset_index()
+arr = numpy.array(df)
 
-g = sns.FacetGrid(df, col='serie', hue='algo', size=4, aspect=1)
-g = g.map(plt.plot, 'taille', 'temps')
-g.set(xscale='log')
-g.set(yscale='log')
-g.add_legend()
-plt.savefig('test_puissance')
+for line in arr:
+    if line[0] == 'counting':
+        line[3] /= line[2]
+    elif line[0] == 'quick':
+        line[3] /= line[2]
+    elif line[0] == 'quickSeuil':
+        line[3] /= line[2]
+    else:
+        line[3] /= line[2]
+d = {'algo': arr[:, 0], 'serie': arr[:, 1], 'taille': arr[:, 2], 'temps': arr[:, 3]}
+newDf = pd.DataFrame(data=d)
+
+
+# g = sns.FacetGrid(newDf, col='serie', hue='algo', size=4, aspect=1)
+# g = g.map(plt.plot, 'taille', 'temps')
+# g.set(xscale='log')
+# g.set(yscale='log')
+# g.add_legend()
+# plt.savefig('test_rapport')
