46723ae5e06ea6510e1a63c177bec5b269761ee5
Unknown <benounicheyacine@gmail.com>
2019-03-23 18:56:16 -0400

    glouton (clean UP)

--- tp2/src/glouton.py
+++ tp2/src/glouton.py
@@ -1,70 +1,92 @@
-import random
-import time
-import csv
 import sys
+import time
 import numpy as np
 
+
 def glouton(emplacement, capacite):
+
     solution = []
-    data = emplacement.copy()
+    disp_data = emplacement
+
     while capacite > 0:
-        data = chooseEmplacement(data, capacite)
-        if (len(data) <= 0):
-            return np.array(solution)
-        rentabilite = np.zeros(len(data))
-        data_index = np.arange(0,len(data))
-        for i in range (len(rentabilite)):
-            rentabilite[i] = data[i][1] / data[i][2]
-        rentabiliteSum = rentabilite.sum()
-        probabilite = rentabilite / rentabiliteSum
-        a = np.random.choice(a=data_index,p=probabilite)
-        capacite -= data[a][2]
-        solution.append(data[a])
-        data.remove(data[a])
-    return np.array(solution)
 
-def chooseEmplacement(emplacement, capacite):
-    result = []  
-    for tempEmplacement in emplacement:
-        if tempEmplacement[2] <= capacite:
-            result.append(tempEmplacement)
-    return result
+        disp_data = [x for x in disp_data if x[2] <= capacite]
+
+        if len(disp_data) == 0:
+            return solution
+
+        rentabilite = np.zeros(len(disp_data))
+        data_index = np.arange(0, len(disp_data))
+
+        for i in range(len(rentabilite)):
+            rentabilite[i] = disp_data[i][1] / disp_data[i][2]
+
+        rentabilite_sum = rentabilite.sum()
+        probabilite = rentabilite / rentabilite_sum
+
+        idx = np.random.choice(a=data_index, p=probabilite)
+
+        capacite -= disp_data[idx][2]
+        solution.append(disp_data[idx])
+        disp_data.remove(disp_data[idx])
+
+    return solution
 
 
+def run(data):
 
-def startAlgo():
-    new_data = fread.read().splitlines()
-    data = []
-    for i in range(1, len(new_data) - 1):
-        index, ri , qi = (int(s) for s in new_data[i].split())
-        data.append((index, ri, qi))
     results = []
-    bestChoice = []
-    sommeMax = 0
-    j=0
-    while j in range (0, 10):
-        start_time = time.time() 
-        choice = glouton(data, int(new_data[i + 1 ]))
-        execution_time = time.time() - start_time
-        results.append(choice)
-        somme = choice[:,1].sum()
-        if sommeMax < somme:
-            bestChoice = choice
-            sommeMax = somme
-        j+=1
-    return bestChoice, execution_time
+    best_sol = []
+    best_sum = 0
 
-if __name__ == "__main__":
-    ex_path = sys.argv[1] # Path de l'exemplaire
+    for j in range(0, 10):
+        choice = glouton(data['emplacements'], data['capacite'])
+        results.append(choice)
+
+        somme = sum([x[1] for x in choice])
+        if best_sum < somme:
+            best_sol = choice
+            best_sum = somme
+
+    return best_sol, best_sum
+
+
+def main():
+
+    data = {
+        'nbEmplacements': 0,
+        'emplacements': [],
+        'capacite': 0
+    }
+
+    ex_path = sys.argv[1]  # Path de l'exemplaire
     options = sys.argv[2:]
-    start_time =time.time()
-    fread = open(ex_path, "r")
-    solution,execution_time = startAlgo()
-    if '-p' in options: # On imprime les nombres triés
-        for elem in solution[:, 0]:
-            print(int(elem))
-        fread.close()
-    if '-t' in options: # On imprime le temps d'exécution
-        #start =time.time()
-        fread.close()
+
+    with open(ex_path, "r") as fp:
+        for i, line in enumerate(fp):
+            if i == 0:
+                data['nbEmplacements'] = int(line.strip())
+            elif i <= data['nbEmplacements']:
+                data['emplacements'].append(tuple(map(int, line.strip().split())))
+            else:
+                data['capacite'] = int(line.strip())
+
+    start_time = time.time()
+
+    solution, max_revenu = run(data)
+
+    execution_time = time.time() - start_time
+
+    if '-p' in options:  # On imprime les nombres triés
+        print('Solution avec revenu = ' + str(max_revenu))
+        for idx, place in enumerate(solution):
+            end = ','
+            if idx == len(solution) - 1:
+                end = '\n'
+            print(place[0], end=end)
+    if '-t' in options:  # On imprime le temps d'exécution
         print(execution_time)
+
+
+if __name__ == '__main__':
+    main()
